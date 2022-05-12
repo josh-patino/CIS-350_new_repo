29b7160402dbc4a0353254504ab4b49279bdcb78
josh-patino <83191880+josh-patino@users.noreply.github.com>
2022-05-11 23:20:02 -0500

    Update PlayerController.cs

--- NavMesh Example Project/Assets/Scripts/PlayerController.cs
+++ NavMesh Example Project/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+/*
+* josh patino
+* assignment 9 
+* player controller, controls the player 
+*/﻿
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
